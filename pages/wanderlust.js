eabfa55eac3a0f78fd9efe69fb51e08c25610ddd
Bobo <kseniushka16@gmail.com>
2019-05-02 10:10:07 +0300

    wanderlust page

---
+++ pages/wanderlust.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const Wanderlust = () => (
+  <Layout>
+    <div>
+      <h1>Wanderlust: travel lover</h1>
+    </div>
+  </Layout>
+);
+
+export default Wanderlust;
