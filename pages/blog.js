22f1e0028259ed8ba1691d044a96eff3e54ac16e
Bobo <kseniushka16@gmail.com>
2019-05-02 10:06:47 +0300

    Clients page

---
+++ pages/blog.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const Blog = () => (
+  <Layout>
+    <div>
+      <h1>Blog Page</h1>
+    </div>
+  </Layout>
+);
+
+export default Blog;
---
+++ pages/clients.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const Clients = () => (
+  <Layout>
+    <div>
+      <h1>For Clients</h1>
+    </div>
+  </Layout>
+);
+
+export default Clients;
